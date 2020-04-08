8bb96da01850d7ccbca8f6778b985ed5e4eacc1e
peyo <peteryyoon@gmail.com>
2020-04-07 17:04:48 -0700

    updated /get.'

--- src/config.js
+++ src/config.js
@@ -2,5 +2,4 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL: process.env.DB_URL || "postgresql:peteryoon:pg9177@localhost/bookmarks",
-  TEST_DB_URL: process.env.TEST_DB_URL || "postgresql:peteryoon:pg9177@localhost/bookmarks-test",
 }
